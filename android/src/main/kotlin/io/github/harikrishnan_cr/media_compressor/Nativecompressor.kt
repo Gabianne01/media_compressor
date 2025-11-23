260218316f8e0f22df4b0b176b1296f5c4c7c378
Gabianne01 <bgagaci@gmail.com>
2025-11-23 22:24:49 +0200

    fix 16px misalignment

--- android/src/main/kotlin/io/github/harikrishnan_cr/media_compressor/Nativecompressor.kt
+++ android/src/main/kotlin/io/github/harikrishnan_cr/media_compressor/Nativecompressor.kt
@@ -10,6 +10,7 @@ import androidx.exifinterface.media.ExifInterface
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.effect.Presentation
 import androidx.media3.effect.ScaleAndRotateTransformation
 import androidx.media3.transformer.Composition
 import androidx.media3.transformer.DefaultEncoderFactory
